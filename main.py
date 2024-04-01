3a7ad79f7fac20047423b6d5ace3d6f08a51b3ae
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2024-04-01 13:34:15 +0300

    added check user func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import FastAPI, Body
 from fastapi.responses import JSONResponse, Response
 
 app = FastAPI()
@@ -11,22 +12,28 @@ def server_log(tag, msg):
 @app.get("/api/auth/{login}&{passwd}")
 async def auth(login, passwd):
     server_log(tag="Login", msg=f"Login={login}, Passwd={passwd}")
-    if login == "admin" and passwd == "admin":
-        return JSONResponse(
-            status_code=200,
-            content={"admin": 1}
-        )
-    elif login == "alexey" and passwd == "alexey":
-        return JSONResponse(
-            status_code=200,
-            content={"admin": 0}
-        )
+    if (login == "admin" and passwd == "admin") or (login == "alexey" and passwd == "alexey"):
+        return Response(status_code=200)
     else:
-        return Response(
-            status_code=403
-        )
+        return Response(status_code=403)
 
 
-@app.get("/")
-async def main():
-    return False
+@app.get("/api/user/check/{login}")
+async def check_admin(login):
+    if login == "admin":
+        return JSONResponse(status_code=200, content={"admin": True})
+    else:
+        return JSONResponse(status_code=200, content={"admin": False})
+
+
+# @app.post("/api/auth/")
+# async def auth(data=Body()):
+#     print(data)
+#     login = data["login"]
+#     passwd = data["passwd"]
+#     server_log(tag="Login", msg=f"Login={login}, Passwd={passwd}")
+#
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
