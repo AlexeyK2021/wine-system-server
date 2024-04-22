78f1d357ae295c03c52cde243414963124ba490c
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2024-04-22 13:30:01 +0300

    endless get data

--- ius/opcua_manager.py
+++ ius/opcua_manager.py
@@ -1,31 +1,44 @@
-import time
 from time import sleep
-
 from opcua import Client
 
 
-class TempHandler(object):
-    def datachange_notification(self, node, val, data):
-        print("Python: New data change event", node, val)
-
-    def event_notification(self, event):
-        print("Python: New event", event)
+def get_value(ip, port, node_id):
+    client = Client(f"opc.tcp://{ip}:{port}")
+    try:
+        client.connect()
+        # root = client.get_root_node()
+        sensor = client.get_node(node_id)
+        value = sensor.get_value()
+        client.disconnect()
+        return value
+    except ConnectionRefusedError:
+        print("Cant connect to sensor")
+        return None
 
 
 if __name__ == '__main__':
-    client = Client("opc.tcp://127.0.0.1:4841")
-    # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
-    while True:
-        try:
-            client.connect()
-
-            root = client.get_root_node()
-            # print("Objects node is: ", root)
-            # print("Children of root are: ", root.get_children())
-            temp_sensor = client.get_node("ns=2;i=6")
-            print(f"Temp={temp_sensor.get_value()}")
-            client.disconnect()
-
-        except ConnectionRefusedError:
-            print("Can't connect to OPC server")
-        sleep(5)
+    ip = "127.0.0.1"
+    port = 4841
+    node_id = "ns=2;i=6"
+    last_val = 0
+    while 1:
+        new_val = get_value(ip, port, node_id)
+        if last_val != new_val:
+            print(new_val)
+            last_val = new_val
+# client = Client("opc.tcp://127.0.0.1:4841")
+# # client = Client("opc.tcp://admin@localhost:4840/freeopcua/server/") #connect using a user
+# while True:
+#     try:
+#         client.connect()
+#
+#         root = client.get_root_node()
+#         # print("Objects node is: ", root)
+#         # print("Children of root are: ", root.get_children())
+#         temp_sensor = client.get_node("ns=2;i=6")
+#         print(f"Temp={temp_sensor.get_value()}")
+#         client.disconnect()
+#
+#     except ConnectionRefusedError:
+#         print("Can't connect to OPC server")
+#     sleep(5)
